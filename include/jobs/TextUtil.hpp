7bbe4bbc3bec46955a36cab0b4319db2f2a29e54
BonKRrr <tigerliu06@gmail.com>
2025-12-18 16:50:20 -0500

    Added hardcoded synonym

--- include/jobs/TextUtil.hpp
+++ include/jobs/TextUtil.hpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3b2619988992f6d5e8f1b579214e0366506c879e349f98d38fcbe2669e8cf24
-size 355
+oid sha256:82765c7de03d744083b8e17ef454dca5b8d50ad38867f9178f1fdab3f062fecb
+size 476
